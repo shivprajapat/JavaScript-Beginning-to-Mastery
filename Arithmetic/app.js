75adb51b9457a24bab2581190b494b29678dcc61
shivprajapat <shivkumarprajapat232@gmail.com>
2022-09-24 14:22:33 +0530

    Arithmetic Operators use

---
+++ Arithmetic/app.js
@@ -0,0 +1,47 @@
+document.write("<h3>Arithmetic Operators</h3>");
+
+// Addition
+// Subtraction
+// Multiplication
+// Exponentiation
+// Division
+// Modulus
+// Increment
+// Decrement
+
+var a = 2;
+var b = 3;
+
+// Addition
+var c = a + b;
+console.log("Addition =>", c);
+
+// Subtraction
+var d = a - b;
+console.log("Subtraction =>", d);
+
+// Multiplication
+var e = a * b;
+console.log("Multiplication =>", e);
+
+// Exponentiation
+var f = a ** b;
+console.log("Exponentiation =>", f);
+
+// Division
+var g = a / b;
+console.log("Division =>", g);
+
+// Modulus
+var l = a % b;
+console.log("Modulus =>", l);
+
+// Increment
+var m = a + b;
+m++;
+console.log("Increment =>", m);
+
+// Decrement
+var n = a - b;
+n--;
+console.log("Decrement =>", n);
